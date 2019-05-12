786497fd3902e1b9a08b392a7506c3db67ee9e93
MInh PHO DUC LE <minh.pdl@gmail.com>
2019-05-12 13:55:16 +0000

    Automatically backed up by Learn

--- printwel.rb
+++ printwel.rb
@@ -1,3 +1,25 @@
 #print wel mass for each user 
 
 
+users = {"Rob", "Sam", "Hum", "Ann", "Hil"]
+
+def greet(user)
+    puts "Hello, #{user}!"
+end
+user.each do |user|
+    greet(user)
+end
+
+
+WINNING_COMBINATIONS = [
+  [0, 1, 2]
+  [3, 4, 5]
+  [6, 7, 8]
+  ]
+  
+  WINNING_COMBINATIONS.each do |winning_combination|
+    winning_combination.each do |number|
+        puts number
+    end
+  end
+  end
