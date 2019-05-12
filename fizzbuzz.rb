d0c6ab16e089144d5f75ddc456e7008a013ec204
MInh PHO DUC LE <minh.pdl@gmail.com>
2019-05-12 13:40:29 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -12,8 +12,10 @@
    (1..100).each do |number|
     if number % 3 === 0 && number % 5 === 0
       puts "FizzBuzz"
-    elseif number % 3 === 100
+    elseif number % 3 === 0
       puts "Fizz"
+    elseif number % 5 === 0
+      puts "Buzz"
     
     
       
